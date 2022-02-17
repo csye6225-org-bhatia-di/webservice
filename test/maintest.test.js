fd5fbbc7f293b20a69b07ef2eef03cdad9da5127
bhatia-di <bhatia.di@northeastern.edu>
2022-02-16 19:27:18 -0500

    Added jest

--- test/maintest.test.js
+++ test/maintest.test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 
 describe('Testing Github workflow', function() {
-    it('Test boolean', function () {
+    it('Test boolean values', function () {
         var b = true;
         assert.equal(true, b);
     });
