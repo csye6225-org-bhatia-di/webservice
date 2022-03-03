3fcd54709ff14bd09c7e8b012986fa1e520ea58c
bhatia-di <bhatia.di@northeastern.edu>
2022-03-03 00:50:58 -0500

    changes for to start

--- packer/package.sh
+++ packer/package.sh
@@ -13,18 +13,21 @@ sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'Vcadd';"
 sudo -u postgres psql -c 'CREATE DATABASE csye6225';
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install -y nodejs
-echo "########## Unzipped #############"
+echo "########## Unzipp begins #############"
 ls
 sleep 30
 cd /tmp/
 echo "Inside tmp/"
 ls -la
 sudo unzip webservice.zip
+sudo chown ec2-user:ec2-user webservice
 cd webservice
+sudo chown ec2-user ./*
 ls -la
 echo "########## Unzipped #############"
 sleep 10
 sudo npm install
+sudo npm install bcrypt
 sudo npm install -g nodemon
 sudo npm run db-migrate
 sudo mv /tmp/webservice.service /etc/systemd/system/webservice.service
