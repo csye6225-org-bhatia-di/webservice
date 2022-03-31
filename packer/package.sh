491939a297e50189c370c3ae8f84b2c8e304870f
bhatia-di <bhatia.di@northeastern.edu>
2022-03-31 17:13:24 -0400

    addded changes

--- packer/package.sh
+++ packer/package.sh
@@ -30,7 +30,7 @@ sudo service codedeploy-agent start
 sudo service codedeploy-agent status
 echo "codedeploy agent status completed"
 echo "########## Unzip begins #############"
-wget https://$aws_code_deploy_bucket_name.s3.$aws_region.amazonaws.com/latest/install
+wget  https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent start
