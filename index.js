bdd51aa4a7c68349931867d7e434badc0c4b9ac1
bhatia-di <bhatia.di@northeastern.edu>
2022-03-31 16:59:39 -0400

    addign changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 //Importing health Routes
 const healthRoute = require('./routes/healthRoute');
-app.use('/healthz', healthRoute);
+app.use('/health', healthRoute);
 //Importing USER Routes
 const userRoute = require('./routes/userRoute');
 app.use('/v1/user', userRoute);
