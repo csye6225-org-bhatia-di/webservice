1b3df8ce1b74380fb2c26e61889aa8c6c658df22
bhatia-di <bhatia.di@northeastern.edu>
2022-04-07 18:50:49 -0400

    added changes

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -6,6 +6,7 @@ sudo chown ec2-user:ec2-user webservice
 pwd
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/tmp/webservice/amazon-cloudwatch-agent.json -s
 cd webservice
+sudo npm i -g npm
 sudo npm install bcrypt
-sudo npm install
+sudo npm i
 sudo npm install -g nodemon
