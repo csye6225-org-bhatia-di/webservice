f9f2899856dfe320f69e2dfe7c7b08645245da1c
bhatia-di <bhatia.di@northeastern.edu>
2022-04-14 16:05:05 -0400

    added new mesg

--- routes/verificationUserRoute.js
+++ routes/verificationUserRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const VerifyUserController = require('../controller/verificationController');
 
-router.get("/", VerifyUserController.verifyUser);
+router.get("/", VerifyUserController.verifyCreatedUser);
 
 module.exports = router;
